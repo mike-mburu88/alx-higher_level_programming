b2f50d6bfb1fde16fd47aa4df104b9c721107af2
root <mngenge@gmail.com>
2023-01-04 18:14:46 -0800

    task 5

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+for num in range(0, 99):
+    print('{0} = {1}'.format(num, hex(num)))
