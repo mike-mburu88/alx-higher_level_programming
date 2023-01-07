93e170e63ff54af8960c8dc05ead250313823b9b
root <mngenge@gmail.com>
2023-01-06 18:50:16 -0800

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b> <a> <operator> <b>")
-        syss.exit(1)
-    ops = ("+": add, "-": sub, "*": mul, "/": div)
+        sys.exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.ext(1)
+        sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
