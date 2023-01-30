adfa35f174987fa0e93917d2fbbda9c5d74ecd8e
root <mngenge@gmail.com>
2023-01-30 01:47:22 -0800

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines the class"""
+
+
+class Rectangle:
+    """Define the class attributes: __size (int): size of a side of the rectangle"""
+    def __init__(self, size):
+        """method to initialize a rectangle args:
+size(int): size of a rectangle side
+Returns: None"""
+        self.__size__ = size
+    
