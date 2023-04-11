2180c263b2d0a0b9efe863d16e26ebcd01a9b0ae
root <mngenge@gmail.com>
2023-04-11 08:59:56 -0700

    correction update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const string of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const string of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(string);
 }
