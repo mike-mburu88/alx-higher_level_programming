9328f29b34a3f10c8f7f6b24d95b021797d6c7c9
root <mngenge@gmail.com>
2023-04-17 03:00:16 -0700

    syntax error change

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host=localhost, user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM STATES ORDER BY 'id'ASC")
+    c.execute("SELECT * FROM states ORDER BY 'id' ASC")
     rows = c.fetchall()
     for row in rows:
         if row[0] == 'N':
