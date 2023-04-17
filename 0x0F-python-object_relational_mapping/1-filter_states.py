b584286d6e729a69a6997d1a701472ba54e06edc
root <mngenge@gmail.com>
2023-04-17 00:27:02 -0700

    added the definition line to task 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,6 +11,7 @@ from sys import argv
 import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
     c.execute("SELECT * FROM STATES ORDER BY 'id'")
     rows = c.fetchall()
     for row in rows:
