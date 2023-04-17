56233209d28c79c94a1c6db3c1a4ed32312c0fec
root <mngenge@gmail.com>
2023-04-16 19:41:07 -0700

    tasks to give output reference of the database

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a codebase that lists all City objects according to respective State
+Takes three arguments
+    username
+    password
+    database name
+Connects to default localhost and default port (3306)
+"""
+
+from sqlalchemy import create_engine
+from sys import argv
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    for city, state in session.query(City, State).filter(
+            State.id == City.state_id).order_by(City.id).all():
+        print("{:d}: {} -> {}".format(city.id, city.name, state.name))
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A script that outputs all city objects
+takes the arguments:
+username
+password
+database name
+Connects to a localhost default port 3306
+"""
+from sqlalchemy import (create_engine)
+from sqlaclhemy.orm import sessionmaker
+from sqlalchemy.orm import aliased
+from model_state import State, Base
+from model_city import City
+from sys import argv
+
+if __name__ == "__main__":
+    Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    result = (session.query(State.name, City.id, City.name).filter(
+        State.id == City.state_id).order_by(City.id).all())
+    for i in result:
+        print("{}: ({:d}) {}".format(i[0], i[1], i[2]))
+    session.close()
