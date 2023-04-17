646e48172fc0af28edc14842d82c1d77c7bc1266
root <mngenge@gmail.com>
2023-04-17 02:56:15 -0700

    error check for codespace

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,7 @@
 """lists all the states that have their names starting with N from a databases
 takes 3 arguments:
 username
-password 
+password
 database name
 connects to a default localhost and port 3306
 """
@@ -10,13 +10,13 @@ connects to a default localhost and port 3306
 from sys import argv
 import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host=localhost, user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM STATES ORDER BY 'id'")
+    c.execute("SELECT * FROM STATES ORDER BY 'id'ASC")
     rows = c.fetchall()
     for row in rows:
-        if row [0] == 'N':
+        if row[0] == 'N':
             print(row)
     c.close()
     db.close()
-        
