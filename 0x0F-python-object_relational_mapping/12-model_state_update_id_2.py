89b2beffb65846109daf2f5834532a4c82ec80fc
root <mngenge@gmail.com>
2023-04-21 14:24:20 -0700

    rectify the modules to be added

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -6,13 +6,15 @@ database name
 password
 """
 
-from sqlalchmey.orm import sessionmaker
-from sqlaclhemy import create_engine
-import sys
-from model_state import State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3], pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                                   pool_pre_ping=True)
     Session = sessionmaker()
     session = Session(bind=engine)
     Base.metadata.create_all(engine)
