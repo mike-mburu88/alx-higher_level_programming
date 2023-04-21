bc22145b201a0c5ad064bb88f1a63fb678a23397
root <mngenge@gmail.com>
2023-04-21 13:37:22 -0700

    code change for the count check

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,21 +7,20 @@ database name
 and connnects to the default localhost at port(3306)
 """
 
-from sqlaclhemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    Session = sessionmaker()
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3], pool_pre_ping=True)
-    session = Session(bind=engine)
-    Base.metadata.createall(engine)
-    instances = session.query(State).filter(State.name == argv[4].all()
-    if instances:
-        for instances in instance:
-            if instance.name == argv[4]:
-                print("{}".format(instance.id))
-    else:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3],
+                                   pool_pre_ping=True))
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    instances = session.query(State).filter(State.name == argv[4])
+    try:
+        print(instances[0].id)
+    except IndexError:
         print("Not found")
-    sessio.close()
