5bd6e348a688de86a0d9e88e942544290964ccd6
root <mngenge@gmail.com>
2023-04-21 09:00:26 -0700

    key changes in file syntax

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,15 +5,15 @@ Its usage can fetch username, password and name
 
 from sqlalchemy import create_engine
 import sys
-import sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-if __name__ = "__main__":
-    emgine = create_engine("mysql+mysqldb://{}:@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3],pool_pre_ping=True)
-
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3],
+                                   pool_pre_ping=True))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for state in session.query(State).order_by(state.id):
-        print("{}: {}".format(state.id, state.name))
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
