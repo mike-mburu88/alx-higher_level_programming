46589ea008e2f46bac4ad742e428671308a70333
root <mngenge@gmail.com>
2023-04-21 03:07:01 -0700

    change to the sql command for execution

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """A code to list all cities within a database that have been joined
 takes 3 arguments:
-username 
+username
 password
 database name
 Connects to a default local host and port 3306
@@ -13,5 +13,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, state.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER by cities.id ASC")
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities\
+            INNER JOIN states ON cities.state_id = states.id\
+            ORDER BY cities.id ASC")
     [print(city) for city in c.fetchall()]
