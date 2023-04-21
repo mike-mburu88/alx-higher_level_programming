bcd2bbe802e02558efc4b3f4484f9d3a4075ac24
root <mngenge@gmail.com>
2023-04-21 12:07:21 -0700

    pycodestyle check complete

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     Session = sessionmaker()
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     session = Session(bind=engine)
     Base.metadata.create_all(engine)
