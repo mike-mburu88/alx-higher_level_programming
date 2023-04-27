ce7b21b37658b532043b178f00e54474c7d58499
root <mngenge@gmail.com>
2023-04-27 15:13:13 -0700

    change to the sort algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,27 +2,26 @@
 """a python program to find a peak in a list of
 unsorted integers"""
 
-if __name__ == "__main__":
-    def find_peak(list_of_integers):
-        """The function to sort a list of integers
-        args: list of integers
-        """
-        if list_of_integers == []:
-            return None
-        listlen = len(list_of_integers)
-        q = int(listlen / 2)
-        x = list_of_integers
+def find_peak(list_of_integers):
+    """The function to sort a list of integers
+    args: list of integers
+    """
+    if list_of_integers == []:
+        return None
+    
+    listlen = len(list_of_integers)
+    q = int(listlen / 2)
+    x = list_of_integers
 
-        if ((q - 1 < 0) && (q + 1 >= listlen)):
-            return x[q]
-        elif q - 1 < 0:
-            return x[q] if  x[q] > x[q + 1] else x[q + 1]
-        elif q + 1 >= listlen:
-            return x[q] if x[q] > x[q - 1] else x[q - 1]
-
-        if x[q - 1] < x[q] > x[q + 1]:
-            return x[q]
+    if q - 1 < 0 and q + 1 >= listlen:
+        return x[q]
+    elif q - 1 < 0:
+        return x[q] if  x[q] > x[q + 1] else x[q + 1]
+    elif q + 1 >= listlen:
+        return x[q] if x[q] > x[q - 1] else x[q - 1]
+    if x[q - 1] < x[q] > x[q + 1]:
+        return x[q]
 
         if x[q + 1] > x[q - 1]:
             return find_peak(x[q:])
-        return find_peak(x[:q]
+        return find_peak(x[:q])
