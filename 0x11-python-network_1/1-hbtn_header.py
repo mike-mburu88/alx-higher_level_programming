b7cd07a235b0c065bbe83eb7e3f65e57aa25da8c
root <mngenge@gmail.com>
2023-04-28 06:06:37 -0700

    value return after url request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a python script that takes a URL and sends
+a request and displays the value X-Request-Id 
+found in the header response
+"""
+
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    link = argv[1]
+    access = urllib.request.Request(link)
+    with urllib.request.urlopen(access) as response:
+        print(dict(response.headers).get("X-Request-Id"))    
