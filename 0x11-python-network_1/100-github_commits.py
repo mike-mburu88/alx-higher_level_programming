f0ea35a29d4f2a79de6ea870afa571efe3cc2e85
root <mngenge@gmail.com>
2023-05-01 07:54:30 -0700

    change to commit code to reflect JSON format

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,6 @@
-#!usr/bin/python3
-"""python code to retrieve the 10 most recent Github commits as specfied on command line
+#!/usr/bin/python3
+"""python code to retrieve the 10 most recent
+ Github commits as specfied on command line
 """
 
 from requests import get
@@ -11,9 +12,7 @@ try:
     s = get(link)
     t = s.json()
     for commit in t[:10]:
-        print('{}: {}'.format(commit.get('sha'),
-                                  commit.get('commit')
-                                  .get('author')
-                                  .get('name')))
+        print('{}: {}'.format(commit.get('sha'), commit.get('commit')
+                              .get('author').get('name')))
 except IndexError as x:
     print(x)
